ec3eb38428f0e6678af824cef9b6e00a4a5b1aa9
Susmitha398 <kvsusi2020@gmail.com>
2025-08-25 19:33:26 +0530

    express install server create

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello from Express with CORS enabled 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
