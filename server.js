2782cb0f6eebdcc81f2bdda9525678b9017463c2
Susmitha398 <kvsusi2020@gmail.com>
2025-08-25 19:38:17 +0530

    i create router section

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from Express with CORS enabled 🚀");
 });
-
+app.get("/movie", (req, res) => {
+  res.send("Hello 🚀");
+});
+app.get("/movie hello", (req, res) => {
+  res.send("Hello pythin 🚀");
+});
 // Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
